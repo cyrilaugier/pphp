7df27b9041fa684592be6afa584c6fc8573df7f2
Cyril Augier <48598522+cyrilaugier@users.noreply.github.com>
2019-09-16 15:53:10 +0200

    Affiche l'article

--- www/article.php
+++ www/article.php
@@ -8,3 +8,9 @@ try {
 } catch (Exception $e) {
 	die('Erreur : ' . $e->getMessage());
 }
+
+$response = $bdd->query('SELECT title, body FROM articles_table WHERE id=1');
+$response = $response->fetch();
+
+echo '<h1>' . $response['title'] . '</h1>';
+echo '<div>' . $response['body'] . '</div>';
