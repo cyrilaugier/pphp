a018f14d85b7a7854b8fa954dd90b8c490a82afa
Cyril Augier <48598522+cyrilaugier@users.noreply.github.com>
2019-09-20 15:04:29 +0200

    Connexion à la bdd depuis create.php

--- www/articles/create.php
+++ www/articles/create.php
@@ -1,7 +1,15 @@
 <?php
 
 // TODO:
-// 1. Connexion à la bdd
 // 2. Récupérer les informations dans $_POST depuis create.php
 // 3. Insérer les données dans la bdd
 // 4. Rediriger vers list.php
+
+$user = 'cyril';
+$passwd = 'password';
+
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=articles_database;charset=utf8', $user, $passwd);
+} catch (Exception $e) {
+	die('Erreur : ' . $e->getMessage());
+}
