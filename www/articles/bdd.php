b6285627969b7a7c5c680650c277d153b8c089f8
Cyril Augier <48598522+cyrilaugier@users.noreply.github.com>
2019-09-20 16:50:59 +0200

    Rangement de bdd.php pour être plus pratique pour la suite

--- www/articles/bdd.php
+++ www/articles/bdd.php
@@ -1,10 +1,12 @@
 <?php
 
-$user = 'cyril';
+$user   = 'cyril';
 $passwd = 'password';
+$host   = 'localhost'; // 127.0.0.1
+$dbName = 'articles_database';
 
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=articles_database;charset=utf8', $user, $passwd);
+    $bdd = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset=utf8', $user, $passwd);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
