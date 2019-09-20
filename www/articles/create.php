01b51124582fbc91229ec33e6a716bd4f7e7f6e5
Cyril Augier <48598522+cyrilaugier@users.noreply.github.com>
2019-09-20 15:17:23 +0200

    Récupère les informations POST envoyés depuis new.php

--- www/articles/create.php
+++ www/articles/create.php
@@ -1,7 +1,6 @@
 <?php
 
 // TODO:
-// 2. Récupérer les informations dans $_POST depuis create.php
 // 3. Insérer les données dans la bdd
 // 4. Rediriger vers list.php
 
@@ -12,4 +11,9 @@ try {
 	$bdd = new PDO('mysql:host=localhost;dbname=articles_database;charset=utf8', $user, $passwd);
 } catch (Exception $e) {
 	die('Erreur : ' . $e->getMessage());
+}
+
+if (isset($_POST['title']) && isset($_POST['body'])) {
+	echo '<h2>' . $_POST['title'] . '</h2>';
+	echo '<p>' . $_POST['body'] . '</p>';
 }
