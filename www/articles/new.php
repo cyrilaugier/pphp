50070397e0a5c3c6846d7750000b57b819a5c50e
Cyril Augier <48598522+cyrilaugier@users.noreply.github.com>
2019-09-20 14:40:17 +0200

    Ajout du formulaire de publication

--- www/articles/new.php
+++ www/articles/new.php
@@ -1,5 +1,19 @@
-<?php
-
-// TODO:
-// 1. Ajouter un formulaire qui permet d'insérer un nouvel article
-// 2. Envoyer l'article vers create.php avec la méthode POST
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<title>new.php</title>
+	</head>
+	<body>
+		<div class="container" />
+			<form id="form" action="create.php" method="post" />
+				<label for="title">Titre :</label>
+				<input type="text" name="title" id="title" value="" />
+				<label for="body">Corps de l'article</label>
+				<textarea name="body" id="body"></textarea>
+				<input type="submit" name="submit" id="submit" value="Publier l'article" />
+			</form>
+		</div>
+	</body>
+</html>
